37a4740cf84dbcf106fe3a6c12f3f23a6764b40a
teefa <gad993813@gmail.com>
2023-09-06 17:36:11 +0300

    new update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\InvoiceController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -22,6 +23,11 @@ Route::get('/',function (){
 Auth::routes(['register'=>false]);
 Route::middleware('admin')->group(function (){
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-    Route::get('/{page}', [AdminController::class,'index']);
+//    Route::get('/{page}', [AdminController::class,'index']);
+    //
+    Route::controller(InvoiceController::class)->group(function (){
+        Route::get('invoice','index')->name('invoice');
+    });
+
 });
 
