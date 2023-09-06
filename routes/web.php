75177b334e3b8a999332ef538f099441658691b8
teefa <gad993813@gmail.com>
2023-09-06 14:59:34 +0300

    new update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/',function (){
+    return view('auth.login');
 });
+Auth::routes(['register'=>false]);
+Route::middleware('admin')->group(function (){
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/{page}', [AdminController::class,'index']);
+});
+
